10effb3bfed28ab134db43dc63009efd2aef855b
Catharina Oliveira <catharinafoliveira@gmail.com>
2021-04-14 00:27:12 -0300

    feat(add cdk files)

--- 2_continuous_integration_and_tests/CDK/app.py
+++ 2_continuous_integration_and_tests/CDK/app.py
@@ -2,17 +2,16 @@
 import os
 
 from aws_cdk import core as cdk
-
+from aws_cdk import aws_s3 as s3
 # For consistency with TypeScript code, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
 
 from cdk.cdk_stack import CdkStack
 
 
-app = core.App()
+app = cdk.App()
 CdkStack(app, "CdkStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
--- 2_continuous_integration_and_tests/CDK/cdk/cdk_stack.py
+++ 2_continuous_integration_and_tests/CDK/cdk/cdk_stack.py
@@ -1,6 +1,6 @@
-from aws_cdk import core
 from aws_cdk import aws_s3 as s3
-
+from aws_cdk import core
+from aws_cdk import core as cdk
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
@@ -8,8 +8,9 @@ from aws_cdk import aws_s3 as s3
 
 class CdkStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        # Recebe como argumento o próprio stack, o nome lógico(ID) e o nome do bucket.
-        s3.Bucket(self,'cdk-bucket-boot-logico', bucket_name=cdk-bucket-boot')
+
         # The code that defines your stack goes here
+        # Recebe como argumento o próprio stack, o nome lógico(ID) e o nome do bucket.
+        s3.Bucket(self,'cdk-bucket-boot-logico', bucket_name='cdk-bucket-boot')
